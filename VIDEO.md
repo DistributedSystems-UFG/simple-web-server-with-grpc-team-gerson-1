d90f9098ad79d42716215a244a50379db42709a3
Guilherme Faleiros <guilhermefaleiros2000@gmail.com>
2022-12-11 16:39:35 -0300

    feat: video

---
+++ VIDEO.md
@@ -0,0 +1,3 @@
+Apresentação do programa no Google Cloud:
+
+https://www.youtube.com/watch?v=XdzLHeb5mu8
--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 docker ps -aq | xargs docker stop | xargs docker rm # Remove all containers
 
 docker run -d -p 50051:50051 guilhermefaleiros/grpc-server:latest | xargs docker logs -f # Run server
-docker run -d guilhermefaleiros/grpc-producer:latest | xargs docker logs -f #Run producer
-docker run -d guilhermefaleiros/grpc-client:latest | xargs docker logs -f  #Run client
+docker run -d guilhermefaleiros/grpc-producer:latest | xargs docker logs -f # Run producer
+docker run -d guilhermefaleiros/grpc-client:latest | xargs docker logs -f  # Run client
